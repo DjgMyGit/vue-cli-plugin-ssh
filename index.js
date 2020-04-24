cf15780e9fb10f9b47caee71a3d9573e6a401b9d
Hank CP <true.cp@gmail.com>
2020-04-24 22:47:31 +0800

    clean remotePath befoe extract zip

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ class SSH2ToServer {
         }
 
         this.uploadShellList = this.config.projectName.startsWith('____') ? [
+            `ls | grep -v ${config.remotePath}/${config.projectName}.zip | parallel rm\n`,
             `unzip -fo ${config.remotePath}/${config.projectName}.zip -d ${config.remotePath}\n`,
             `rm -rf ${config.remotePath}/${config.projectName}.zip\n`,
             `exit\n`
